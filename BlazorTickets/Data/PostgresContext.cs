50ae9eb25b4dd33b07e324f2e39fef0cf40e61a7
carlos.blancocastro <carlos.blancocastro@ENGR9JCP5S3>
2024-02-07 16:58:41 -0700

    scaffolded!!!!

---
+++ BlazorTickets/Data/PostgresContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorTickets.Data;
+
+public partial class PostgresContext : DbContext
+{
+    public PostgresContext()
+    {
+    }
+
+    public PostgresContext(DbContextOptions<PostgresContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Event> Events { get; set; }
+
+    public virtual DbSet<Ticket> Tickets { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql("Name=Postgres");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .HasPostgresExtension("pg_catalog", "azure")
+            .HasPostgresExtension("pg_catalog", "pgaadauth");
+
+        modelBuilder.Entity<Event>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("event_pkey");
+
+            entity.ToTable("event");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.DateOfEvent).HasColumnName("date_of_event");
+            entity.Property(e => e.EventName)
+                .HasMaxLength(100)
+                .HasColumnName("event_name");
+        });
+
+        modelBuilder.Entity<Ticket>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("ticket_pkey");
+
+            entity.ToTable("ticket");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.EventId).HasColumnName("event_id");
+            entity.Property(e => e.IsScanned).HasColumnName("is_scanned");
+            entity.Property(e => e.UserEmail)
+                .HasMaxLength(100)
+                .HasColumnName("user_email");
+
+            entity.HasOne(d => d.Event).WithMany(p => p.Tickets)
+                .HasForeignKey(d => d.EventId)
+                .HasConstraintName("ticket_event_id_fkey");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ BlazorTickets/Data/Ticket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorTickets.Data;
+
+public partial class Ticket
+{
+    public int Id { get; set; }
+
+    public int? EventId { get; set; }
+
+    public string? UserEmail { get; set; }
+
+    public bool? IsScanned { get; set; }
+
+    public virtual Event? Event { get; set; }
+}
